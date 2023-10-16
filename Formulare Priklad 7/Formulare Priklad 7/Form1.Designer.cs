3f4dcea1719d01cd7320e717967fa044fc26ff6b
Antoninstolfa <stolfaa.04@spst.eu>
2023-10-16 13:53:32 +0200

    Hotovo + možnost zakázat zvětšování a změnšování formu.

--- Formulare Priklad 7/Formulare Priklad 7/Form1.Designer.cs
+++ Formulare Priklad 7/Formulare Priklad 7/Form1.Designer.cs
@@ -36,37 +36,69 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(216, 150);
+            this.button1.Location = new System.Drawing.Point(61, 93);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Zpracovat";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(166, 40);
+            this.numericUpDown1.Location = new System.Drawing.Point(61, 14);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(52, 20);
             this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
             // 
             // numericUpDown2
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(166, 66);
+            this.numericUpDown2.Location = new System.Drawing.Point(61, 40);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
             this.numericUpDown2.Name = "numericUpDown2";
             this.numericUpDown2.Size = new System.Drawing.Size(52, 20);
             this.numericUpDown2.TabIndex = 2;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(166, 93);
+            this.textBox1.Location = new System.Drawing.Point(61, 67);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 3;
@@ -74,7 +106,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(123, 42);
+            this.label1.Location = new System.Drawing.Point(18, 16);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(37, 13);
             this.label1.TabIndex = 4;
@@ -83,7 +115,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(121, 68);
+            this.label2.Location = new System.Drawing.Point(16, 42);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(39, 13);
             this.label2.TabIndex = 5;
@@ -92,17 +124,39 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(129, 96);
+            this.label3.Location = new System.Drawing.Point(24, 70);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(31, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "Text:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(182, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 71);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Změnit barvu";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(182, 93);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(103, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Změna velikosti:";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -129,6 +183,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- Formulare Priklad 7/Formulare Priklad 7/Form1.cs
+++ Formulare Priklad 7/Formulare Priklad 7/Form1.cs
@@ -21,13 +21,53 @@ namespace Formulare_Priklad_7
         {
             Form form2 = new Form();
 
+            int x = (int)numericUpDown1.Value;
+            int y = (int)numericUpDown2.Value;
+
+            form2.Size = new Size(x, y);
+            form2.Text = textBox1.Text;
+            form2.BackColor = colorDialog1.Color;
+
             Button zpet = new Button();
+            zpet.DialogResult = DialogResult.OK;
             zpet.Location = new Point(10,10);
             zpet.Size = new Size(60, 20);
             zpet.Text = "Zpět";
+            zpet.BackColor = Color.White;
+            zpet.ForeColor = Color.Black;
 
+            Button konec = new Button();
+            konec.DialogResult = DialogResult.Cancel;
+            konec.Location = new Point(10, 35);
+            konec.Size = new Size(60, 20);
+            konec.Text = "Konec";
+            konec.BackColor = Color.White;
+            konec.ForeColor = Color.Black;
 
+            if(checkBox1.Checked == true)
+            {
+                form2.FormBorderStyle = FormBorderStyle.FixedDialog;
+            }
 
+            form2.Controls.Add(zpet);
+            form2.Controls.Add(konec);
+
+            this.Hide();
+
+            if(form2.ShowDialog() == DialogResult.OK)
+            {
+                form2.Close();
+                this.Show();    
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
         }
     }
 }
