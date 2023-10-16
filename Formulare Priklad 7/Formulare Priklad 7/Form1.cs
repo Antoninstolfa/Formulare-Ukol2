a26020c99a9296a26468d5ea31127612d4a6710e
Antoninstolfa <97514759+Antoninstolfa@users.noreply.github.com>
2023-10-16 10:43:36 +0200

    Dodělat doma

---
+++ Formulare Priklad 7/Formulare Priklad 7/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formulare_Priklad_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form form2 = new Form();
+
+            Button zpet = new Button();
+            zpet.Location = new Point(10,10);
+            zpet.Size = new Size(60, 20);
+            zpet.Text = "Zpět";
+
+
+
+        }
+    }
+}
